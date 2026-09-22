3d7395a02c6c7d34128b95260118d279e6414aff
Marceloou <marcelo.henriquerosaf@gmail.com>
2026-09-22 16:26:42 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,5 @@
+function calcularAreaRetangulo(base, altura) {
+    return base * altura;
+}
+let resultado = calcularAreaRetangulo(6, 7);
+console.log(resultado); // Questão fácil, só tive dúvida na pontuação, com ; e {}
